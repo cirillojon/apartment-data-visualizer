9dbc3dfe7f62801b92ec0e56bff2b33e483c8987
Jon <jonathancirillo@outlook.com>
2023-08-07 10:54:27 -0400

    improved code structure

--- core/scrape.py
+++ core/scrape.py
@@ -1,8 +1,14 @@
-import requests, os
+import requests
+import os
+import psycopg2
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-load_dotenv()
 
+# Constants
+URL = "https://www.amli.com/apartments/seattle/ballard-apartments/amli-mark24/floorplans"
+
+# Load environment variables
+load_dotenv()
 POSTGRES_PASS = os.getenv('POSTGRES_PASS')
 
 def extract_data(element, class_name):
@@ -30,72 +36,75 @@ def extract_bedroom_count(s):
 def extract_price(s):
     """Extracts price from a string and converts to float."""
     if s:
-        # Removing dollar sign and any non-numeric characters except for digits and dots
         numbers = ''.join(filter(lambda x: x.isdigit() or x == '.', s.replace("$", "")))
         if numbers:
             return float(numbers)
     return None
 
-URL = "https://www.amli.com/apartments/seattle/ballard-apartments/amli-mark24/floorplans"
-response = requests.get(URL)
-soup = BeautifulSoup(response.content, 'html.parser')
+def scrape_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    floorplans = soup.find_all('div', class_='floorplan-title-container')
 
-floorplans = soup.find_all('div', class_='floorplan-title-container')  # Each floorplan's starting point
+    scraped_data = []
 
-# List to store scraped data
-scraped_data = []
-
-for floorplan in floorplans:
-    data = {}
-    data['floorplan_name'] = floorplan.find('h3', class_='floorplan-title').text if floorplan.find('h3', class_='floorplan-title') else None
-    data['available_units'] = extract_integer_from_string(floorplan.find('h4', class_='floorplan-available-units').text) if floorplan.find('h4', class_='floorplan-available-units') else None
+    for floorplan in floorplans:
+        data = {}
+        data['floorplan_name'] = floorplan.find('h3', class_='floorplan-title').text if floorplan.find('h3', class_='floorplan-title') else None
+        data['available_units'] = extract_integer_from_string(floorplan.find('h4', class_='floorplan-available-units').text) if floorplan.find('h4', class_='floorplan-available-units') else None
+        
+        # Directly finding the parent information container for the given floorplan title
+        info_container = floorplan.find_next('div', class_='floorplan-information-container')
+        
+        data['bedrooms'] = extract_bedroom_count(extract_data(info_container, 'floorplan-info floorplan-beds'))
+        data['bathrooms'] = extract_data(info_container, 'floorplan-info floorplan-baths')
+        data['size_range'] = extract_data(info_container, 'floorplan-info floorplan-size')
+        data['availability_date'] = extract_data(info_container, 'floorplan-info floorplan-availability')
+        data['price'] = extract_price(extract_data(info_container, 'floorplan-info floorplan-pricing'))
+        
+        scraped_data.append(data)
     
-    # Directly finding the parent information container for the given floorplan title
-    info_container = floorplan.find_next('div', class_='floorplan-information-container')
+    return scraped_data
+
+def insert_data_to_db(data_list):
+    try:
+        with psycopg2.connect(dbname="apartments_db", user="postgres", password=POSTGRES_PASS, host="localhost", port="5432") as connection:
+            with connection.cursor() as cursor:
+                
+                # Check if table exists and create if not
+                cursor.execute("""
+                CREATE TABLE IF NOT EXISTS apartments (
+                    id SERIAL PRIMARY KEY,
+                    floorplan_name VARCHAR(255),
+                    available_units INT,
+                    bedrooms INT,
+                    bathrooms INT,
+                    size_range VARCHAR(255),
+                    availability_date DATE,
+                    price MONEY
+                );
+                """)
+
+                for data in data_list:
+                    cursor.execute("""
+                        INSERT INTO apartments (floorplan_name, available_units, bedrooms, bathrooms, size_range, availability_date, price)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s);
+                    """, (data['floorplan_name'], data['available_units'], data['bedrooms'], data['bathrooms'], data['size_range'], data['availability_date'], data['price']))
+
+        print("Data inserted successfully!")
+
+    except psycopg2.Error as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    scraped_data = scrape_data(URL)
     
-    data['bedrooms'] = extract_bedroom_count(extract_data(info_container, 'floorplan-info floorplan-beds'))
-    data['bathrooms'] = extract_data(info_container, 'floorplan-info floorplan-baths')
-    data['size_range'] = extract_data(info_container, 'floorplan-info floorplan-size')
-    data['availability_date'] = extract_data(info_container, 'floorplan-info floorplan-availability')
-    data['price'] = extract_price(extract_data(info_container, 'floorplan-info floorplan-pricing'))
-    
-    scraped_data.append(data)
-
-    # Print the data
-    for key, value in data.items():
-        print(f"{key}: {value}")
-    print("--------------------------")
-
-import psycopg2
-
-try:
-    # Connecting to the DB
-    connection = psycopg2.connect(
-        dbname="apartments_db",
-        user="postgres",
-        password=POSTGRES_PASS,
-        host="localhost",
-        port="5432"
-    )
-    cursor = connection.cursor()
-
-    # Inserting data into the database
     for data in scraped_data:
-        cursor.execute("""
-            INSERT INTO apartments (floorplan_name, available_units, bedrooms, bathrooms, size_range, availability_date, price)
-            VALUES (%s, %s, %s, %s, %s, %s, %s);
-        """, (data['floorplan_name'], data['available_units'], data['bedrooms'], data['bathrooms'], data['size_range'], data['availability_date'], data['price']))
+        for key, value in data.items():
+            print(f"{key}: {value}")
+        print("--------------------------")
+        
+    insert_data_to_db(scraped_data)
 
-    # Committing transaction
-    connection.commit()
-    print("Data inserted successfully!")
-
-except psycopg2.Error as e:
-    print(f"An error occurred: {e}")
-
-finally:
-    # Close the cursor and connection, even if an error occurred.
-    if cursor:
-        cursor.close()
-    if connection:
-        connection.close()
+if __name__ == "__main__":
+    main()
