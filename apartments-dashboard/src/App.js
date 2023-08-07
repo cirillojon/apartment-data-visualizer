c7d7cc47427f54ea3499ce118da8f19acb9894c5
Jon <jonathancirillo@outlook.com>
2023-08-07 14:53:17 -0400

    working display on same graph

--- apartments-dashboard/src/App.js
+++ apartments-dashboard/src/App.js
@@ -8,16 +8,20 @@ function App() {
 
     useEffect(() => {
         fetch("http://127.0.0.1:5000/api/apartments")
-        .then(res => res.json())
-        .then(data => {
-            const formattedData = data.map(apartment => ({
-                ...apartment,
-                x: apartment.size_range.includes('→') ? (parseInt(apartment.size_range.split(' → ')[0]) + parseInt(apartment.size_range.split(' → ')[1])) / 2 : parseInt(apartment.size_range),
-                y: apartment.price ? parseFloat(apartment.price.replace('$', '').replace(',', '')) : 0,
-            }));
-            setApartments(formattedData);
-        })
-        .catch(err => console.error("Error fetching apartment data:", err));
+            .then(res => res.json())
+            .then(data => {
+                const formattedData = data.map(apartment => ({
+                    x: apartment.size_range.includes('→') ? (parseInt(apartment.size_range.split(' → ')[0]) + parseInt(apartment.size_range.split(' → ')[1])) / 2 : parseInt(apartment.size_range),
+                    y: apartment.price ? parseFloat(apartment.price.replace('$', '').replace(',', '')) : 0,
+                    bedrooms: apartment.bedrooms,
+                    floorplanName: apartment.floorplan_name,
+                    availableUnits: apartment.available_units,
+                    sizeRange: apartment.size_range,
+                    complex_name: apartment.complex_name // Add the complex_name
+                }));
+                setApartments(formattedData);
+            })
+            .catch(err => console.error("Error fetching apartment data:", err));
     }, []);
 
     const complexNames = Array.from(new Set(apartments.map(data => data.complex_name)));
@@ -28,49 +32,44 @@ function App() {
 
     const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
+            const data = payload[0].payload;
             return (
                 <div className="custom-tooltip">
-                    <p className="label">{`Complex Name: ${payload[0].payload.complex_name}`}</p>
-                    <p className="desc">{`Price: $${payload[0].payload.y}`}</p>
-                    <p className="sqft">{`Sqft: ${payload[0].payload.size_range}`}</p>
-                    <p className="units">{`Available Units: ${payload[0].payload.available_units}`}</p>
+                    <p className="label">{`Complex Name: ${data.complex_name || 'N/A'}`}</p>
+                    <p className="desc">{`Price: $${data.y || 'N/A'}`}</p>
+                    <p className="sqft">{`Sqft: ${data.sizeRange || 'N/A'}`}</p>
+                    <p className="units">{`Available Units: ${data.availableUnits || 'N/A'}`}</p>
                 </div>
             );
         }
         return null;
     };
+    
 
     return (
         <div className="App">
             {apartments.length && <div className="legend">
                 {complexNames.map(complexName => (
                     <span key={complexName} className="legend-item">
-                        <span className="color-box" style={{backgroundColor: getComplexColor(complexName)}}></span>
+                        <span className="color-box" style={{ backgroundColor: getComplexColor(complexName) }}></span>
                         {complexName}
                     </span>
                 ))}
             </div>}
             
-            {complexNames.map(complexName => {
-                const filteredData = apartments.filter(data => data.complex_name === complexName);
-
-                return (
-                    <div key={complexName} style={{ marginBottom: 20 }}>
-                        <h4 style={{ fontSize: 24 }}>{complexName}</h4>
-                        <ScatterChart width={500} height={300}>
-                            <CartesianGrid />
-                            <XAxis type="number" dataKey="x" name="Square Footage">
-                                <Label value="Square Footage" position="bottom" />
-                            </XAxis>
-                            <YAxis type="number" dataKey="y" name="Price" unit="$">
-                                <Label value="Price" angle={-90} position="left" />
-                            </YAxis>
-                            <Tooltip content={<CustomTooltip />} cursor={{ strokeDasharray: '3 3' }} />
-                            <Scatter data={filteredData} fill={getComplexColor(complexName)} />
-                        </ScatterChart>
-                    </div>
-                );
-            })}
+            <ScatterChart width={500} height={300}>
+                <CartesianGrid />
+                <XAxis type="number" dataKey="x" name="Square Footage">
+                    <Label value="Square Footage" position="bottom" />
+                </XAxis>
+                <YAxis type="number" dataKey="y" name="Price" unit="$">
+                    <Label value="Price" angle={-90} position="left" />
+                </YAxis>
+                <Tooltip content={<CustomTooltip />} cursor={{ strokeDasharray: '3 3' }} />
+                {complexNames.map(complexName => (
+                    <Scatter key={complexName} data={apartments.filter(ap => ap.complex_name === complexName)} fill={getComplexColor(complexName)} name={complexName} />
+                ))}
+            </ScatterChart>
         </div>
     );
 }
